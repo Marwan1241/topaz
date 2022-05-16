29ae19f5f7ed12b8c801f16742f701d0b3281629
Marwan Hawash <marwanmagdi.2001@gmail.com>
2022-05-16 15:56:47 +0200

    finished the contact page and about page , still needs some finishing touches

--- app/views/pages/about.php
+++ app/views/pages/about.php
@@ -6,17 +6,31 @@ class About extends view
   {
     $title = $this->model->title;
     $data = $this->model->data;
-
-    require APPROOT . '/views/inc/header.php';
-    $text = <<<EOT
-    <div class="jumbotron jumbotron-fluid">
-    <div class="container">
-      <h1 class="display-4 title"> $title</h1>
-      <h2 class="display-4 text"> $data</h>
-    </div>
-  </div>
-EOT;
-    echo $text;
-    require APPROOT . '/views/inc/footer.php';
   }
 }
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?php echo URLROOT; ?>css/about.css">
+  <title>About Us</title>
+</head>
+<body style="background-color: white !important;">
+  <?php require APPROOT . '/views/inc/header.php'; ?>
+  <header class="aboutHeader">
+    <h1>About Topaz</h1>
+  </header>
+
+  <main class="aboutMain">
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Recusandae voluptatem cumque excepturi commodi harum officiis, atque voluptate quia quos quod quidem laudantium accusamus, voluptatibus mollitia alias. Assumenda, aut cupiditate! Excepturi? Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, maiores qui eos cupiditate repellat nulla? Nulla id quae, quisquam omnis quaerat reiciendis adipisci soluta maxime debitis beatae nostrum saepe eum.</p>
+  </main>
+  
+
+  <?php require APPROOT . '/views/inc/footer.php'; ?>
+</body>
+</html>
--- app/views/pages/contact.php
+++ app/views/pages/contact.php
@@ -7,29 +7,52 @@ class Contact extends view
     $title = $this->model->title;
     $subtitle = $this->model->subtitle;
 
-    require APPROOT . '/views/inc/header.php';
-    $text = <<<EOT
-    <div class="jumbotron jumbotron-fluid">
-    <div class="container">
-      <h1 class="display-4 title"> $title</h1>
-      <h2 class="display-4 text"> $subtitle</h>
-      <br/>
-      <br/>
-      <form action="contact.php" method="post">
-        Your name<br>
-        <input type="text" name="cf_name"><br>
-        Your e-mail<br>
-        <input type="text" name="cf_email"><br>
-        Message<br>
-        <textarea name="cf_message"></textarea><br>
-        <input type="submit" value="Send">
-        <input type="reset" value="Clear">
-      </form>
-    </div>
-  </div>
-EOT;
-    echo $text;
-    require APPROOT . '/views/inc/footer.php';
   }
 }
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" /> 
+  <link rel="stylesheet" href="<?php echo URLROOT; ?>css/contact.css">
+  <title>Contact Us</title>
+</head>
+<body>
+  <?php  require APPROOT . '/views/inc/header.php'; ?>
+
+  <main>
+  <header>
+    <h1>We'd Love to hear from you !</h1>
+    <p>Shoot us a message here , or you can email us at topaz-support@email.com</p>
+    <br><br>
+    <form class="form-contact" action="">
+      <label for="nameInput">Enter Your Name</label>
+      <br>
+      <input type="text" id="nameInput">
+      <br><br>
+      <label for="emailInput">Enter Your Email</label>
+      <br>
+      <input type="email" id="emailInput">
+      <br><br>
+      <button class="btn-primaryy">Submit</button>
+    </form>
+  </header>
+ 
+
+    <aside>
+      <h2>Our Social Media</h2>
+      <i class="fa-brands fa-facebook">   Facebook</i>
+      <i class="fa-brands fa-instagram">   Instagram</i>
+      <i class="fa-brands fa-twitter">   Twitter</i>
+    </aside>
+  </main>
+      
+  <?php  require APPROOT . '/views/inc/footer.php'; ?>
+</body>
+</html>
 
