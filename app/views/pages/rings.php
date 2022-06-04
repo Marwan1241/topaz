6cd5614bea9309b18e93c0d8044eeb0510c0471e
Marwan Hawash <marwanmagdi.2001@gmail.com>
2022-06-04 18:04:23 +0200

    updated a small code snippet in rings

--- app/views/pages/rings.php
+++ app/views/pages/rings.php
@@ -38,7 +38,7 @@ class Rings extends view
           $new_row =<<<EOD
             <tr>
               <form method="post" action="$URLroot/pages/rings">
-                <td class="product-image"><img src="$image"></td>
+                <td class="product-img"><img src="$image"></td>
                 <td class="product-name">$name</td>
                 <td class="product-desc">$description</td>
                 <td class="product-price"> $ $price</td>
